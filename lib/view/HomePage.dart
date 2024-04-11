9044cb3f5e116b6fae17809a06c86ec1248897d3
itsmedona <donashaji254@gmail.com>
2024-04-11 08:50:32 +0530

    Frst Commit

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_list/view/widgets/RecipeCard.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.restaurant_menu),
+          SizedBox(width: 10),
+          Text('Food Recioe')
+        ],
+      ),),
+     body: RecipeCard(
+          title: 'My recipe',
+          rating: '4.9',
+          cookTime: '30 min',
+          thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+        ));
   }
 }
